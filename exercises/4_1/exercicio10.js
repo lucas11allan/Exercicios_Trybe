52268434464a18c33cbda9faeed549f411619902
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-13 11:53:55 -0300

    Criaçao de um codigo que gera os movimentos de uma peça de xadrez apos a inserçao de um nome de peça valido

---
+++ exercises/4_1/exercicio10.js
@@ -0,0 +1,33 @@
+let pecaXadrez = "";
+
+pecaXadrez.toLocaleLowerCase();
+
+switch(pecaXadrez){
+  case 'peão':
+    console.log("Um movimento para frente e ataque na diagonal");
+    break;
+
+    case 'torre':
+    console.log("Movimentos livres em linha reta");
+    break;
+
+    case 'cavalo':
+    console.log("Movimento em 'L'");
+    break;
+
+    case 'bispo':
+    console.log("Movimentos livres na diagonal");
+    break;
+
+    case 'rainha':
+    console.log("Movimentos livres em linha reta e na diagonal");
+    break;
+
+    case 'rei':
+    console.log("Um movimento em linha reta ou na diagonal");
+    break;
+
+    default:
+    console.log("Insira um nome de peça válido");
+    break;
+}
