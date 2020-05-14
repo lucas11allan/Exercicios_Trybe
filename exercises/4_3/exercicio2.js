cee3a1c9a320377f33c72bc6a0a3d1ea07e33ca1
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-14 15:28:13 -0300

    exercicio2

--- exercises/4_3/exercicio2.js
+++ exercises/4_3/exercicio2.js
@@ -1,12 +1,9 @@
-let n = 2;
+let n = 7;
 let asteriscos = "";
 let i = 0;
-let j = 0;
 
 for(i = 1; i <= n; i += 1){
   asteriscos += "*";
-}
-
-for(j = 1; j <= n; j += 1){
   console.log(asteriscos);
 }
+
