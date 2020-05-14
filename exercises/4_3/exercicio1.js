347cb35b50ecfb06dc302286db4160ecf69d91c2
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-14 15:26:08 -0300

    Criaçao de um codigo que gera um quadrado de asteriscos com n valores

---
+++ exercises/4_3/exercicio1.js
@@ -0,0 +1,12 @@
+let number = 2;
+let asteriscos = "";
+let i = 0;
+let j = 0;
+
+for(i = 1; i <= number; i += 1){
+  asteriscos += "*";
+}
+
+for(j = 1; j <= number; j += 1){
+  console.log(asteriscos);
+}
