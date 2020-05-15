d838b22aacba19f30ac7dd959ff4f7d4b307072b
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-15 20:21:50 -0300

    inicio exercicio 2-5

---
+++ exercises/4_4/exercicio-2-5.js
@@ -0,0 +1,21 @@
+let numeros = [2, 3, 2, 5, 8, 2, 3];
+let numeroMaisRepete = 0;
+
+function retornaNumMaisRepete(arrayNumeros){
+  let vezesRepete = 0;
+  let pxmNumero = 0;
+  
+  for(let indice in arrayNumeros){
+    if(arrayNumeros[indice] = pxmNumero){
+      vezesRepete += ;
+    }
+  }
+
+
+  return numeroMaisRepete;
+}
+
+
+retornaNumMaisRepete(numeros);
+
+console.log(numeroMaisRepete);
