f93d90a0679619b49c134c842a358713c6968886
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-14 22:49:50 -0300

    otimizaçao do exercicio 4

--- exercises/4_3/exercicio4.js
+++ exercises/4_3/exercicio4.js
@@ -1,25 +1,25 @@
-let n = 9;
+let n = 4;
 let asterisco = "";
 let espaco = "";
 let impressao = "";
 let i = 0;
 let j = 0;
 let k = 0;
+let l = 0;
+let x = 0;
 
 for (i = 1; i <= n; i += 2){
   asterisco = "";
   espaco = "";
+  l += 1;
 
-  for (j = n; j >= 1; j -= 1){
-    if(j<=i){
-      asterisco += "*";
-    }
+  for (k = (n - 2); k >= i; k -= 2){
+      espaco += " ";
+      x += 1;
   }
 
-  for (k = n; k >= 1; k -= 2){
-    if(k>i){
-      espaco += " ";
-    }
+  for (j = ((n % 2) + n); j <= (n + i); j += 1){
+    asterisco += "*"; 
   }
 
   impressao = espaco + asterisco + espaco;
