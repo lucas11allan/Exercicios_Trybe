cbb84b889fc835eff634ec8e38f65a9eda8688cd
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-16 19:28:06 -0300

    Finalizaçao do exercicio bonus

--- exercises/4_4/exercicio-bonus.js
+++ exercises/4_4/exercicio-bonus.js
@@ -1,8 +1,9 @@
-let arrayNumRomanos = ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX','XXI','XXII','XXIII','XXIV','XXV','XXVI','XXVII','XXVIII','XXIX','XXX','XXXI','XXXII','XXXIII','XXXIV','XXXV','XXXVI','XXXVII','XXXVIII','XXXIX','XL','XLI','XLII','XLIII','XLIV','XLV','XLVI','XLVII','XLVIII','XLIX','L','LI','LII','LIII','LIV','LV','LVI','LVII','LVIII','LIX','LX','LXI','LXII','LXIII','LXIV','LXV','LXVI','LXVII','LXVIII','LXIX','LXX','LXXI','LXXII','LXXIII','LXXIV','LXXV','LXXVI','LXXVII','LXXVIII','LXXIX','LXXX','LXXXI','LXXXII','LXXXIII','LXXXIV','LXXXV','LXXXVI','LXXXVII','LXXXVIII','LXXXIX','XC','XCI','XCII','XCIII','XCIV','XCV','XCVI','XCVII','XCVIII','XCIX','C'];
+let arrayNumRomanos = ['XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX','XXI','XXII','XXIII','XXIV','XXV','XXVI','XXVII','XXVIII','XXIX','XXX','XXXI','XXXII','XXXIII','XXXIV','XXXV','XXXVI','XXXVII','XXXVIII','XXXIX','XL','XLI','XLII','XLIII','XLIV','XLV','XLVI','XLVII','XLVIII','XLIX','L','LI','LII','LIII','LIV','LV','LVI','LVII','LVIII','LIX','LX','LXI','LXII','LXIII','LXIV','LXV','LXVI','LXVII','LXVIII','LXIX','LXX','LXXI','LXXII','LXXIII','LXXIV','LXXV','LXXVI','LXXVII','LXXVIII','LXXIX','LXXX','LXXXI','LXXXII','LXXXIII','LXXXIV','LXXXV','LXXXVI','LXXXVII','LXXXVIII','LXXXIX','XC','XCI','XCII','XCIII','XCIV','XCV','XCVI','XCVII','XCVIII','XCIX','C', 'MCMXCIX', 'MMCMXCIX'];
 let numConvertido = 0;
 let arrayArabico = [];
 let arrayNumerosConvertidos = [];
 
+//Essa função converte uma string de números Romanos em um número Arábico
 function converteRomanoArabico(numRomano){
   //Limpa as variáveis numConvertido e arrayArabico caso a função seja usada mais de uma vez
   numConvertido = 0;
@@ -51,12 +52,15 @@ function converteRomanoArabico(numRomano){
 
     }else if(arrayArabico[posicao] < arrayArabico[(posicao - 1)]){
       numConvertido += arrayArabico[posicao];
+    }
 
-    //Faz as comparações do número atual com o próximo
+    //Faz as comparações do número atual com o próximo, sendo necessário quando o número romano está precedido e sucedido por números maiores que o atual
     if(arrayArabico[(posicao + 1)] === ""){
       break;
     }else if(arrayArabico[posicao] < arrayArabico[(posicao + 1)] && arrayArabico[posicao] < arrayArabico[(posicao-1)]){
-      numConvertido += (arrayArabico[(posicao +1)] - arrayArabico[posicao]);
+      numConvertido += (arrayArabico[(posicao +1)] - arrayArabico[posicao]) - arrayArabico[posicao];
+
+      //Nesse caso especial o próximo número já foi computado, então posicao recebe um incremento para 'pulá-lo'
       posicao += 1;
 
     }
@@ -67,9 +71,10 @@ function converteRomanoArabico(numRomano){
 
 }
 
+
 //usa a função de conversão para converter todos os números de um array
 function converteArrayRomanoArabico(arrayRomanos){
-  for(let i = 0; i <= arrayRomanos.length; i += 1){
+  for(let i = 0; i < arrayRomanos.length; i += 1){
     converteRomanoArabico(arrayRomanos[i]);
     arrayNumerosConvertidos.push(numConvertido);
   }
@@ -81,4 +86,3 @@ function converteArrayRomanoArabico(arrayRomanos){
 converteArrayRomanoArabico(arrayNumRomanos);
 
 console.log(arrayNumerosConvertidos);
-
