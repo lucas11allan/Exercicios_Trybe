ae052d16bf3d411bedaaa30304c67ec7099029f7
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-06-18 19:21:50 -0300

    Refatoraçao exercise 5

--- exercises/8_3/exercise5.js
+++ exercises/8_3/exercise5.js
@@ -3,10 +3,8 @@ const assert = require('assert')
 const myList = [1, 2, 3]
 
 // escreva swap abaixo
-const swap = (array) => {
-  const [a, b, c] = array;
-  return [c, b, a];
-}
+const swap = ([a,b,c]) => [c, b, a];
+
 const swappedList = swap(myList)
 
 assert.equal(swappedList[0], 3)
