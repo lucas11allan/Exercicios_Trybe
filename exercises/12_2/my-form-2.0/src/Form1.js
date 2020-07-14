6ee2a822941d4bfc2f5e438d97fcaea1164ff408
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-14 18:53:29 -0300

    exercicio sendo feito

--- exercises/12_2/my-form-2.0/src/Form1.js
+++ exercises/12_2/my-form-2.0/src/Form1.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const listaEstados = ['MG', 'SP', 'RJ'];
+
 class Form1 extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +13,7 @@ class Form1 extends React.Component {
       cidade: '',
       estado: '',
       tipo: '',
+      
     };
   }
 
@@ -23,10 +26,40 @@ class Form1 extends React.Component {
     return (
       <div>
         <form>
-          <label>
-            Nome:{' '}
-            <input type='text' name='nome' value={this.state.nome} onChange={this.handleChange} />
-          </label>
+          <fieldset>
+            <label>
+              Nome:{' '}
+              <input required type='text' name='nome' value={this.state.nome} onChange={this.handleChange} />
+            </label>
+            <label>
+              Email:{' '}
+              <input required type='email' name='email' value={this.state.email} onChange={this.handleChange} />
+            </label>
+            <label>
+              CPF:{' '}
+              <input required type='text' name='cpf' value={this.state.cpf} onChange={this.handleChange} />
+            </label>
+            <label>
+              Endereço:{' '}
+              <input required type='text' name='endereco' value={this.state.endereco} onChange={this.handleChange} />
+            </label>
+            <label>
+              Cidade:{' '}
+              <input required type='text' name='cidade' value={this.state.cidade} onChange={this.handleChange} />
+            </label>
+            <label>
+              Estado:{' '}
+              <select required value={this.state.estado} onChange={this.handleChange}>
+                {listaEstados.map((estado, i) => (
+                  <option key={i} value={estado}>{estado}</option>
+                ))}
+              </select>
+            </label>
+          </fieldset>
+          <fieldset>
+
+          </fieldset>
+          <button type="submit">Enviar</button>
         </form>
       </div>
     );
