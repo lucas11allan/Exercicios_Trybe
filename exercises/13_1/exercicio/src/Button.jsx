305f419313fceb64663b12e7ab602cb1298bcbda
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-17 18:32:20 -0300

    Construçao dos componentes do exercicio

---
+++ exercises/13_1/exercicio/src/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Button = (props) => {
+  const { showComponent, isDisable, content } = props;
+  return (
+    <button
+      type="button"
+      disabled={isDisable}
+      onClick={() => showComponent()}
+    >
+      {content}
+    </button>
+  )
+}
+
+export default Button;
